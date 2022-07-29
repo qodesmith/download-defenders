5ae10a6bf111889fc8b6a876da63be665135a671
Qodesmith <theqodesmith@gmail.com>
2022-07-29 10:56:44 -0400

    Checking files for 0 bytes. Fancier logging - showing individual file progress.

--- downloadPodcastData.js
+++ downloadPodcastData.js
@@ -3,6 +3,7 @@ import downloadYouTube from './downloadYouTube.js'
 import filenamify from 'filenamify'
 import chalk from 'chalk'
 import fs from 'fs-extra'
+import {resourceLimits} from 'worker_threads'
 
 function namey(fileName, extension) {
   const name = filenamify(fileName)
@@ -10,19 +11,21 @@ function namey(fileName, extension) {
 }
 
 export default async function downloadPodcastData({page, sectionsData}) {
-  for (let i = 0; i < sectionsData.length; i++) {
+  const sectionDataLength = sectionsData.length
+
+  for (let i = 0; i < sectionDataLength; i++) {
     const {dir, podcasts, shouldSkip, folderName} = sectionsData[i]
 
     if (shouldSkip) {
       console.log(
-        chalk.gray.bold('Section exists (skipping):'),
+        chalk.gray.bold(`Skipping section ${i + 1} of ${sectionDataLength}:`),
         chalk.gray(folderName)
       )
       continue
     } else {
       console.log(
         chalk.cyan.bold(
-          `Downloading section ${i + 1} of ${sectionsData.length}:`
+          `Downloading section ${i + 1} of ${sectionDataLength}:`
         ),
         folderName
       )
@@ -30,30 +33,26 @@ export default async function downloadPodcastData({page, sectionsData}) {
 
     for (let j = 0; j < podcasts.length; j++) {
       const {url, name} = podcasts[j]
+
+      // We'll look to download 3 files for each podcast. Create those names.
       const [mp3FileName, pdfFileName, mp4FileName] = ['mp3', 'pdf', 'mp4'].map(
-        extension => {
-          return namey(name, extension)
-        }
+        extension => namey(name, extension)
       )
-      const hasFileObj = {
-        mp3FileName: fs.existsSync(`${dir}/${mp3FileName}`),
-        pdfFileName: fs.existsSync(`${dir}/${pdfFileName}`),
-        mp4FileName: fs.existsSync(`${dir}/${mp4FileName}`),
-      }
-      const hasAllFiles = Object.values(hasFileObj).every(val => val)
+
+      // If the file exists AND it has a size we'll want to skip downloading it.
+      const [mp3FileExists, pdfFileExists, mp4FileExists] = [
+        mp3FileName,
+        pdfFileName,
+        mp4FileName,
+      ].map(fileName => {
+        const fullPath = `${dir}/${fileName}`
+        return fs.existsSync(fullPath) && fs.statSync(fullPath).size
+      })
 
       // Skip navigating to the page altogether if we already have all 3 files.
-      if (hasAllFiles) continue
+      if (mp3FileExists && pdfFileExists && mp4FileExists) continue
 
       await page.goto(url)
-
-      // https://stackoverflow.com/questions/32938213/is-there-a-way-to-erase-the-last-line-of-output
-      // Clear each time except the first iteration.
-      if (j !== 0) {
-        process.stdout.moveCursor(0, -1) // up one line
-        process.stdout.clearLine(1) // from cursor to end
-      }
-      console.log(chalk.cyan(`${j + 1}/${podcasts.length}`), chalk.gray(name))
 
       /*
         There are 3 things we want from each podcast episode page:
@@ -99,24 +98,101 @@ export default async function downloadPodcastData({page, sectionsData}) {
         }),
       ])
 
-      await Promise.allSettled([
-        !hasFileObj[mp3FileName] &&
-          downloadFile({dir, fileName: mp3FileName, url: mp3Url}),
-        !hasFileObj[pdfFileName] &&
-          downloadFile({dir, fileName: pdfFileName, url: pdfUrl}),
-        !hasFileObj[mp4FileName] &&
-          downloadYouTube({dir, fileName: mp4FileName, url: youTubeUrl}),
-      ]).then(([mp3Status, pdfStatus, mp4Status]) => {
-        if (mp3Status.status === 'rejected') {
-          console.log(chalk.red(`Failed to download file:`, mp3FileName))
-        }
-        if (pdfStatus.status === 'rejected') {
-          console.log(chalk.red(`Failed to download file:`, pdfFileName))
-        }
-        if (mp4Status.status === 'rejected') {
-          console.log(chalk.red(`Failed to download file:`, mp4FileName))
+      const promiseBools = []
+      const promiseCb = i => () => {
+        promiseBools[i] = true
+        logProgress({
+          promiseBools,
+          current: j + 1,
+          total: podcasts.length,
+          name,
+        })
+      }
+      const promises = [
+        !mp3FileExists &&
+          mp3Url &&
+          downloadFile({dir, fileName: mp3FileName, url: mp3Url}).then(
+            promiseCb(0)
+          ),
+        !pdfFileExists &&
+          pdfUrl &&
+          downloadFile({dir, fileName: pdfFileName, url: pdfUrl}).then(
+            promiseCb(1)
+          ),
+        !mp4FileExists &&
+          youTubeUrl &&
+          downloadYouTube({dir, fileName: mp4FileName, url: youTubeUrl}).then(
+            promiseCb(2)
+          ),
+      ]
+
+      // Massage the promiseBools array for the 1st logging attempt below.
+      promises.forEach((item, i) => {
+        if (item instanceof Promise) {
+          // Indicates an incomplete download in progress.
+          promiseBools[i] = false
+        } else {
+          // Indicates file not available to download.
+          promiseBools[i] = null
         }
       })
+
+      logProgress({promiseBools, current: j + 1, total: podcasts.length, name})
+
+      await Promise.all(promises).then(() =>
+        checkSizes({dir, fileNames: [mp3FileName, pdfFileName, mp4FileName]})
+      )
     }
   }
+}
+
+/*
+  Data needed:
+    * Each promise "complete"
+      * null - no promise (no file to download)
+      * true - promise to download file
+    * Current iteration
+    * Total iterations
+    * Podcast name
+*/
+function logProgress({promiseBools, current, total, name}) {
+  const openBracket = chalk.cyan.bold('[')
+  const closedBracket = chalk.cyan.bold(']')
+  const comma = chalk.cyan(',')
+  const empty = chalk.cyan('▯')
+  const full = chalk.cyan('▮')
+  const blocks = `${full.repeat(current)}${empty.repeat(total - current)}`
+  const fileTypes = ['mp3', 'pdf', 'mp4']
+    .map((ext, i) => {
+      const value = promiseBools[i]
+      if (value === null) return
+      return value ? chalk.cyan(ext) : chalk.gray(ext)
+    })
+    .filter(Boolean)
+    .join(comma)
+
+  // https://stackoverflow.com/questions/32938213/is-there-a-way-to-erase-the-last-line-of-output
+  // Clear each time except the first iteration.
+  if (current !== 1) {
+    process.stdout.moveCursor(0, -2) // Up 2 lines
+    process.stdout.clearScreenDown()
+  }
+
+  console.log(`${openBracket}${blocks}${closedBracket}`)
+  console.log(
+    chalk.cyan(`${current}/${total}`),
+    `${openBracket}${fileTypes}${closedBracket}`,
+    chalk.gray(name)
+  )
+}
+
+function checkSizes({dir, fileNames}) {
+  fileNames.forEach(name => {
+    const fullPath = `${dir}/${name}`
+    const fileSize = fs.statSync(fullPath).size
+
+    if (fileSize === 0) {
+      throw new Error(`0 byte file size: ${name}`)
+    }
+  })
 }
