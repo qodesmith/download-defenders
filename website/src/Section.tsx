dd050cca7115024586513eb71a68800fc6a42587
Qodesmith <theqodesmith@gmail.com>
2022-08-13 18:40:14 -0400

    Started on a custom website.

---
+++ website/src/Section.tsx
@@ -0,0 +1,49 @@
+import type {SectionType} from '../../websiteMiddlewares.js'
+
+import styled from 'styled-components'
+
+export default function Section({
+  section,
+  num,
+}: {
+  section: SectionType
+  num: number
+}) {
+  return (
+    <Container>
+      <H2>{section.sectionName}</H2>
+      <div>{section.episodes.length} episodes</div>
+      <Index>{num}</Index>
+    </Container>
+  )
+}
+
+const Container = styled.section`
+  width: 300px;
+  height: 300px;
+  border: 1px solid #333;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  padding: 10px;
+  border-radius: 20px;
+  position: relative;
+`
+
+const H2 = styled.h2`
+  margin-bottom: 0;
+`
+
+const Index = styled.div`
+  position: absolute;
+  top: 50;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  z-index: -1;
+  opacity: 0.025;
+  font-size: 14em;
+  line-height: 0;
+  font-weight: bold;
+`
---
+++ websiteMiddlewares.ts
@@ -0,0 +1,101 @@
+import fs from 'fs-extra'
+import path from 'node:path'
+
+export type DefendersDataType = SectionType[]
+export type SectionType = {
+  sectionName: string // Foundations of Christian Doctrine
+  episodes: EpisodeType[]
+}
+export type EpisodeType = {
+  title: string // Foundations of Christian Doctrine (Part 1)! Why Study Christian Doctrine
+  fileNames: {
+    mp3?: string // 01 - Foundations of Christian Doctrine (Part 1)! Why Study Christian Doctrine.mp3
+    mp4?: string
+    pdf?: string
+  }
+}
+
+/*
+  TODO:
+  Can this be more strongly typed?
+  Can we infer specific values from DefendersDataType?
+
+  {[fileName]: filePath}
+*/
+type DefendersDataMap = Record<string, string>
+
+const fileTypes = ['mp3', 'mp4', 'pdf'] as const
+const rootPath = path.resolve('./Defenders Series 3')
+
+function createDefendersData(): DefendersDataType {
+  return fs
+    .readdirSync(rootPath, {withFileTypes: true})
+    .reduce((dataArr, dirent) => {
+      if (!dirent.isDirectory()) return dataArr
+
+      const sectionName = dirent.name
+      const sectionPath = path.resolve(rootPath, sectionName)
+      const sectionFiles = fs.readdirSync(sectionPath)
+      const sectionFilesSet = new Set(sectionFiles)
+      const episodes: EpisodeType[] = []
+
+      for (let i = 0; i < sectionFiles.length; i++) {
+        const fileName = sectionFiles[i]
+        const episodeNumber = Number(fileName.slice(0, 2))
+        const isEpisodeFile = fileTypes.some(ext =>
+          fileName.endsWith(`.${ext}`)
+        )
+
+        /*
+          Only process files related to the episode (i.e. skip '.DStore', etc.).
+          We process each episode's files in batch, just once. Skip otherwise.
+        */
+        if (!isEpisodeFile || episodes[episodeNumber - 1]) continue
+
+        /*
+          Remove the prefixed number and the suffixed file extension:
+            * 5 - Each episode starts with 5 characters, such as '01 - '
+            * 4 - Each episode ends with 4 characters, such as '.mp4'
+        */
+        const fileParts = path.parse(fileName)
+        const fileWithoutExt = fileParts.name
+        const episodeTitle = fileWithoutExt.slice(5) // Remove the number prefix.
+        const episode: EpisodeType = {
+          title: episodeTitle,
+          fileNames: fileTypes.reduce((acc: EpisodeType['fileNames'], ext) => {
+            acc[ext] = `${fileWithoutExt}.${ext}`
+            return acc
+          }, {}),
+        }
+
+        episodes.push(episode)
+      }
+
+      dataArr.push({sectionName: sectionName.slice(5), episodes})
+      return dataArr
+    }, [] as DefendersDataType)
+}
+
+function createDefendersFileMap(): DefendersDataMap {
+  return fs
+    .readdirSync(rootPath, {withFileTypes: true})
+    .reduce((acc: DefendersDataMap, dirent) => {
+      if (!dirent.isDirectory()) return acc
+
+      const sectionName = dirent.name
+      const sectionPath = path.resolve(rootPath, sectionName)
+
+      fs.readdirSync(sectionPath).forEach(file => {
+        const isDesiredFile = fileTypes.some(type => file.endsWith(`.${type}`))
+        if (!isDesiredFile) return
+
+        const filePath = path.resolve(sectionPath, file)
+        acc[file] = filePath
+      })
+
+      return acc
+    }, {})
+}
+
+export const defendersData = createDefendersData()
+export const defendersFileMap = createDefendersFileMap()
