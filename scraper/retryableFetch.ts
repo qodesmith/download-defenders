d2349daff0dad140e3ea1de4d4fcf7a8969d6ffe
Qodesmith <theqodesmith@gmail.com>
2024-01-07 16:35:20 -0500

    Implemented retryableFetch to avoid 429 (too many requests) error.

---
+++ scraper/retryableFetch.ts
@@ -0,0 +1,35 @@
+import chalk from 'chalk'
+
+export function retryableFetch(
+  url: string,
+  {timeout = 100, maxRetries = 3}: {timeout?: number; maxRetries?: number} = {}
+) {
+  let tries = 0
+  function executeFetch() {
+    return fetch(url)
+      .then(res => {
+        tries++
+        if (res.ok || tries > maxRetries) return res
+
+        return new Promise(resolve => {
+          setTimeout(resolve, timeout)
+        }).then(() => {
+          console.log(chalk.gray('  [then] retrying fetch...'))
+          return executeFetch()
+        })
+      })
+      .catch(err => {
+        tries++
+        if (tries > maxRetries) return err
+
+        return new Promise(resolve => {
+          setTimeout(resolve, timeout)
+        }).then(() => {
+          console.log(chalk.gray('  [catch] retrying fetch ...'))
+          return executeFetch()
+        })
+      })
+  }
+
+  return executeFetch()
+}
