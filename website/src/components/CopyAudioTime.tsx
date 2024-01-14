25508025c1698f6f5270eabe6695e870d02653a6
Qodesmith <theqodesmith@gmail.com>
2024-01-14 06:57:26 -0500

    UX / JS improvements to copy component

--- website/src/components/CopyAudioTime.tsx
+++ website/src/components/CopyAudioTime.tsx
@@ -1,4 +1,4 @@
-import {useCallback} from 'react'
+import {useCallback, useEffect, useState} from 'react'
 import * as stylex from '@stylexjs/stylex'
 
 const styles = stylex.create({
@@ -8,6 +8,12 @@ const styles = stylex.create({
     justifyContent: 'center',
     gap: '0.5em',
     userSelect: 'none',
+    '--icon-border-color': '#aaa',
+  },
+  subContainer: {
+    ':hover': {
+      '--icon-border-color': 'white',
+    },
   },
   cursorPointer: {
     cursor: 'pointer',
@@ -23,7 +29,7 @@ const styles = stylex.create({
   copyIcon: {
     width: '0.75em',
     height: '1em',
-    border: '1px solid #aaa',
+    border: '1px solid var(--icon-border-color)',
     borderRadius: '0.2em',
     background: '#555',
     position: 'absolute',
@@ -36,6 +42,9 @@ const styles = stylex.create({
     transform: 'rotate(-20deg)',
     right: '0.2em',
   },
+  transparent: {
+    opacity: 0.5,
+  },
 })
 
 type Props = {
@@ -44,6 +53,30 @@ type Props = {
 }
 
 export function CopyAudioTime({audioRef, episodeTitle}: Props) {
+  return (
+    <>
+      {/* COPY TIME & EPISODE LINK */}
+      <CopyToClipboard
+        includeEpisodeLink
+        audioRef={audioRef}
+        episodeTitle={episodeTitle}
+      />
+
+      {/* COPY TIME ONLY */}
+      <CopyToClipboard audioRef={audioRef} episodeTitle={episodeTitle} />
+    </>
+  )
+}
+
+type CopyToClipboardProps = Props & {
+  includeEpisodeLink?: boolean
+}
+
+function CopyToClipboard({
+  includeEpisodeLink,
+  audioRef,
+  episodeTitle,
+}: CopyToClipboardProps) {
   const handleOnClick = useCallback(() => {
     const audioEl = audioRef.current
     if (!audioEl) return
@@ -54,52 +87,52 @@ export function CopyAudioTime({audioRef, episodeTitle}: Props) {
     const displayTime = `${minutes}:${seconds}`
     const url = new URL(window.location.href)
     url.searchParams.set('t', `${time}`)
-    const finalText = `[${displayTime} - ${episodeTitle}](${url})`
+    const finalText = includeEpisodeLink
+      ? `[${displayTime} - ${episodeTitle}](${url})`
+      : `[${displayTime}](${url})`
 
     navigator.clipboard.writeText(finalText)
+  }, [includeEpisodeLink])
+  const [isPressed, setIsPressed] = useState(false)
+  const handleOnMouseDown = useCallback(() => {
+    setIsPressed(true)
   }, [])
 
-  const handleOnClick2 = useCallback(() => {
-    const audioEl = audioRef.current
-    if (!audioEl) return
+  useEffect(() => {
+    const onMouseUp = () => setIsPressed(false)
+    document.addEventListener('mouseup', onMouseUp)
 
-    const time = Math.round(audioEl.currentTime)
-    const minutes = Math.floor(time / 60)
-    const seconds = `${time - minutes * 60}`.padStart(2, '0')
-    const displayTime = `${minutes}:${seconds}`
-    const url = new URL(window.location.href)
-    url.searchParams.set('t', `${time}`)
-    const finalText = `[${displayTime}](${url})`
-
-    navigator.clipboard.writeText(finalText)
+    return () => document.removeEventListener('mouseup', onMouseUp)
   }, [])
 
   return (
-    <>
-      <div {...stylex.props(styles.container, styles.cursorInitial)}>
-        <div
-          onClick={handleOnClick}
-          {...stylex.props(styles.container, styles.cursorPointer)}
-        >
-          <div {...stylex.props(styles.iconContainer)}>
-            <div {...stylex.props(styles.copyIcon)} />
-            <div {...stylex.props(styles.copyIcon, styles.copyIconRear)} />
-          </div>
-          <span>Copy time & episode link</span>
+    // CONTAINER - spans the width of the page
+    <div
+      {...stylex.props(
+        styles.container,
+        styles.cursorInitial,
+        isPressed && styles.transparent
+      )}
+    >
+      {/* SUB CONTAINER - spans only the width of the content */}
+      <div
+        onClick={handleOnClick}
+        {...stylex.props(
+          styles.container,
+          styles.subContainer,
+          styles.cursorPointer
+        )}
+        onMouseDown={handleOnMouseDown}
+      >
+        {/* ICON */}
+        <div {...stylex.props(styles.iconContainer)}>
+          <div {...stylex.props(styles.copyIcon)} />
+          <div {...stylex.props(styles.copyIcon, styles.copyIconRear)} />
         </div>
+
+        {/* TEXT */}
+        <span>Copy time{includeEpisodeLink ? ' & episode link' : ''}</span>
       </div>
-      <div {...stylex.props(styles.container, styles.cursorInitial)}>
-        <div
-          onClick={handleOnClick2}
-          {...stylex.props(styles.container, styles.cursorPointer)}
-        >
-          <div {...stylex.props(styles.iconContainer)}>
-            <div {...stylex.props(styles.copyIcon)} />
-            <div {...stylex.props(styles.copyIcon, styles.copyIconRear)} />
-          </div>
-          <span>Copy time</span>
-        </div>
-      </div>
-    </>
+    </div>
   )
 }
