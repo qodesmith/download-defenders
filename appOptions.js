15befbce9e2c05665435713846a4ad462d18822b
Qodesmith <theqodesmith@gmail.com>
2024-01-16 09:17:47 -0500

    Removed old files and unused npm packages.

--- appOptions.js
+++
@@ -1,6 +0,0 @@
-import commandLineArgs from 'command-line-args'
-
-const optionDefinitions = [{name: 'data', type: Boolean}]
-const appOptions = commandLineArgs(optionDefinitions)
-
-export default appOptions
