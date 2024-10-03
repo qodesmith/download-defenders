4adeb5b11c2717a8b6d1effe2aa58dbe692c0973
Qodesmith <theqodesmith@gmail.com>
2024-10-03 11:17:09 -0400

    Updated type

--- scraper/chunkArray.ts
+++ scraper/chunkArray.ts
@@ -1,4 +1,4 @@
-export function chunkArray<T>(arr: T[], size): T[][] {
+export function chunkArray<T>(arr: T[], size: number): T[][] {
   return Array.from({length: Math.ceil(arr.length / size)}, (v, i) =>
     arr.slice(i * size, i * size + size)
   )
